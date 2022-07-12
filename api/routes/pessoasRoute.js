2f466b0021711f640d142741ea22227a5f1c1fed
jedslim <87735876+jedslim@users.noreply.github.com>
2022-07-12 17:33:00 -0300

    Initial commit

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,17 @@
+const { Router } = require ('express');
+const PessoasController = require ('../controllers/PessoaController');
+
+const router = Router();
+
+router.get('/pessoas', PessoasController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoasController.pegaUmaPessoa);
+router.post('/pessoas', PessoasController.criaPessoa);
+router.put('/pessoas/:id', PessoasController.atualizaPessoa);
+router.delete('/pessoas/:id', PessoasController.apagaPessoa);
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoasController.pegaUmaMatricula);
+router.post('/pessoas/:estudanteId/matricula', PessoasController.criaMatricula);
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoasController.atualizaMatricula);
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoasController.apagaMatricula);
+
+
+module.exports = router;
